21ba65a2237907bd70c192901e994dc0c3fb5304
John Mburu <johnnkonge2020@gmail.com>
2025-08-18 07:37:07 +0300

    added multi_location app

--- backend/customers/models.py
+++ backend/customers/models.py
@@ -8,12 +8,20 @@ TIERS = [
     {'platinum':'Platinum'},
 ]
 
+class MembershipTier(models.Model):
+    name = models.CharField(max_length=100,unique=True)
+    description = models.TimeField(blank=True)
+    tenant = models.ForeignKey('tenants.Tenant', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+
 class Customer(models.Model):
     full_name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=20)
     address = models.CharField(max_length=100)
-    membership_tier = models.CharField(max_length=20, choices=TIERS)
+    membership_tier = models.ForeignKey(MembershipTier,on_delete=models.SET_NULL)
     member_since = models.DateField(auto_now_add=True)
     last_visit = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -21,3 +29,5 @@ class Customer(models.Model):
     
     def __str__(self):
         return self.full_name
+    
+    
