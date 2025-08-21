09531017b1c0b98fac67931c235ef9350d15b9bb
John Mburu <johnnkonge2020@gmail.com>
2025-08-21 19:16:09 +0300

    updating the app

---
+++ backend/products/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import *
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['name','description','category','supplier','cost_price','selling_price','initial_stock','current_stock','minimum_stock_level']
