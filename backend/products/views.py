684406b91e18f20024b6d6f399144db186e9ac18
John Mburu <johnnkonge2020@gmail.com>
2025-08-22 03:35:49 +0300

    created products APIs

--- backend/products/views.py
+++ backend/products/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from .models import *
 from .serializers import *
+from django.shortcuts import get_object_or_404
 
 class ProductListCreateAPIView(APIView):
     """
@@ -23,3 +24,38 @@ class ProductListCreateAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.error)
+    
+class ProductRetrieveUpdateDestroyAPIView(APIView):
+    
+    """
+    helper function to get an object instance
+    """
+    def get_object(self,pk):
+        return get_object_or_404(Product,pk=pk)
+    
+    """
+    GET METHOD: To retrieve an object by ID 
+    """
+    def get(self, request,pk):
+        product = self.get_object(pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    """
+    PUT/PATCH METHOD: To make partial update to a single product
+    """
+    def put(self, request, pk):
+        product = self.get_object(pk)
+        serializer = ProductSerializer(product, data = request.data, partial = True)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.error)
+    """
+    DELETE METHOD: To delete a single product by ID
+    """
+    def delete(self,request,pk):
+        product = self.get_object(pk)
+        # serializer = ProductSerializer(product)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
