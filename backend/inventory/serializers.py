851ebb477a4fd7003ef9f502ddde826ba7d85277
John Mburu <johnnkonge2020@gmail.com>
2025-08-28 07:56:47 +0300

    created the products APIs in the inventory app

--- backend/inventory/serializers.py
+++ backend/inventory/serializers.py
@@ -9,6 +9,20 @@ class productSerializer(serialziers.ModelSerializer):
         fields = ['id','name','sku','categories','price','supplier','stock_status', 'tenant']
         read_only_fields = ['tenant']
         
+    def create(self, validated_data):
+        current_stock = validated_data.pop("current_stock", 0)
+        min_stock = validated_data.pop("min_stock", 0)
+        max_stock = validated_data.pop("max_stock", 0)
+        product = super().create(validated_data)
+        Inventory.objects.create(
+            product=product,
+            branch=None,  
+            current_stock=current_stock,
+            min_stock=min_stock,
+            max_stock=max_stock,
+            tenant=product.tenant
+        )
+        
 class InventorySerializer(serialziers.ModelSerializer):
     
     class Meta:
--- backend/inventory/views.py
+++ backend/inventory/views.py
@@ -1,3 +1,67 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import *
+from django.shortcuts import get_object_or_404
+from .models import *
 
-# Create your views here.
+
+class ProductListCreateAPIView(APIView):
+    """
+    GET METHOD: To get all products in our inventory
+    """
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = productSerializer(products, many = True)
+        return Response(serializer.data, status = status.HTTP_200_OK)
+        
+    """
+        POST METHOD: TO create a new product and enlist it to our inventory
+    """
+    
+    def post(self, request):
+        serializer = productSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save(tenant = request.user.tenant)
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+    
+class ProductRetrieveUpdateDestroyAPIView(APIView):
+    
+    """
+    helper method to get a single instace of a product
+    """
+    
+    def get_object(self, pk):
+        return get_object_or_404(Product, pk = pk)
+    
+    """
+      GET METHOD: To retrieve a product by its ID  
+    """
+    
+    def get(self, request, pk):
+        product = self.get_object(pk)
+        serializer = productSerializer(product)
+        return Response(serializer.data, status =status.HTTP_200_OK)
+    
+    """
+        PUT METHOD: To edit/update a product instance
+    """
+    
+    def put(self, request, pk):
+        product = self.get_object(pk)
+        serializer = productSerializer(product, data = request.data,partial =True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+    
+    """
+    DELETE METHOD: To delete a product instance
+    """
+    
+    def delete(self, pk):
+        product = self.get_object(pk)
+        product.delete()
+        return Response({"message":"Product deleted"},status = status.HTTP_204_NO_CONTENT)
