5dacf901de6c96b0e450960ec72ffcac02b1a979
John Mburu <johnnkonge2020@gmail.com>
2025-09-10 21:06:07 +0300

    added git ignore

--- backend/payments/serializers.py
+++ backend/payments/serializers.py
@@ -8,4 +8,5 @@ class PaymentSerializer(serializers.ModelSerializer):
         fields = ['id','customer_name','customer_email','customer_phone',
                   'payment_type','total_amount','total_vat','transaction_id',
                   'mpesa_reference','card_last4','timestamp','order','tenant']
-        read_only_fields = ['tenant','mpesa_reference','card_last4','transaction_id','timestamp']
+        read_only_fields = ['tenant','mpesa_reference','card_last4',
+                            'transaction_id','timestamp']
