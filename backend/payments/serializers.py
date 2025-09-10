28a064b6131bb3c3b2c77cdfb28cc3ecbfc424d8
John Mburu <johnnkonge2020@gmail.com>
2025-09-10 21:01:50 +0300

    crafted the payments serializer

---
+++ backend/payments/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework.serializers import serializers
+from .models import *
+
+class PaymentSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Payments
+        fields = ['id','customer_name','customer_email','customer_phone',
+                  'payment_type','total_amount','total_vat','transaction_id',
+                  'mpesa_reference','card_last4','timestamp','order','tenant']
+        read_only_fields = ['tenant','mpesa_reference','card_last4','transaction_id','timestamp']
