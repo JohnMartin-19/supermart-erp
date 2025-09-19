b7820bb0399ae096efe6c2bcc9d107a5bd8d018a
John Mburu <johnnkonge2020@gmail.com>
2025-09-19 09:44:55 +0300

    mapped URLS for the payroll

--- backend/payroll/urls.py
+++ backend/payroll/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('employees/', EmployeeListCreateAPIView.as_view()),
     path('employees/<int:pk>/', EmployeeRetrieveUpdateDestroyAPIView.as_view()),
+    path('process_payroll/', PayrollProcessView.as_view()),
 ]
